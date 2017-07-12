51d073b53b9bcd2c673dd3821d96e25edf4481c7
Laszlo Arnould <laszlo.arnould@gmail.com>
2017-07-12 14:48:04 +0200

    contact's controller done

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,10 +1,29 @@
 class ContactsController < ApplicationController
   def new
+    @contact = Contact.new
   end
 
   def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      flash[:success] = "Your demand has been transmitted, we'll get at you shortly"
+      redirect_to root_path
+    else
+      flash[:danger] = "An error occured, please try again"
+      render :new
+    end
   end
 
   def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    redirect_to root_path
   end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:email, :object, :description)
+  end
+
 end
