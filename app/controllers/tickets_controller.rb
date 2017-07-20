b842a52bf33fe22ba296fae68c5e9f8343357fad
Laszlo Arnould <laszlo.arnould@gmail.com>
2017-07-20 13:56:47 +0200

    routes optimisation, seed sections

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,10 +1,13 @@
 class TicketsController < ApplicationController
+  before_action :find_ticket, only: [:edit, :update, :show]
+
   def new
     @ticket = Ticket.new
   end
 
   def create
     @ticket = Ticket.new(ticket_params)
+    @ticket.save
   end
 
   def edit
@@ -17,12 +20,17 @@ class TicketsController < ApplicationController
   end
 
   def index
+    @tickets = Ticket.all
   end
 
   private
 
   def ticket_params
-    params.require(:ticket).permit(:email, :object, :description)
+    params.require(:ticket).permit(:object, :description)
+  end
+
+  def find_ticket
+    Ticket.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,60 +1,19 @@
 Rails.application.routes.draw do
 
-
-  get 'tickets/new'
-
-  get 'tickets/create'
-
-  get 'tickets/edit'
-
-  get 'tickets/update'
-
-  get 'tickets/show'
-
-  get 'tickets/index'
-
-  get 'messages/new'
-
-  get 'messages/create'
-
-  get 'messages/edit'
-
-  get 'messages/update'
-
-  get 'messages/show'
-
-  get 'messages/destroy'
-
-  get 'conversations/create'
-
-  get 'conversations/show'
-
-  get 'conversations/index'
-
-  get 'conversations/destroy'
-
-  get 'contacts/new'
-
-  get 'contacts/create'
-
-  get 'contacts/destroy'
-
-  get 'workers/create'
-
-  get 'workers/update'
-
-  get 'workers/show'
-
-  get 'workers/destroy'
-
-  get 'sections/create'
-
-  get 'sections/update'
-
-  get 'sections/show'
-
-  get 'sections/destroy'
+  root to: "pages#home"
 
   devise_for :users
-  root to: "pages#home" # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :workers, except: [:new, :edit, :index]
+
+  resources :sections, except: [:new, :edit, :index]
+
+  resources :tickets, except: :destroy
+
+  resources :contacts, only: [:new, :create, :destroy]
+
+  resources :conversations, except: [:new, :edit, :update] do
+    resources :messages, except: :index
+  end
+
 end
